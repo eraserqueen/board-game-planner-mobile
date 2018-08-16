ce6e233f70c5ddd707ca78394c51ca4adf6391cb
Dominique Rolin <drolin@thoughtworks.com>
2018-08-16 12:37:59 +1000

    ejected expo

--- src/utils/ApiSessionManager.js
+++ src/utils/ApiSessionManager.js
@@ -1,8 +1,6 @@
-import Expo from "expo";
 import fetch from "cross-fetch";
 
-const {manifest} = Expo.Constants;
-const host = manifest.packagerOpts.dev ? manifest.debuggerHost.split(':').shift().concat(':3000') : 'http://produrl.com';
+const host = 'http://localhost:3000';
 
 
 const unAuthClient = {
